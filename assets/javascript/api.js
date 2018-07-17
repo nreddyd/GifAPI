7395cf4bf9205bc8a8ba1ebb62d1c47138085fe3
Neeraja <neerajareddy24@gmail.com>
2018-07-17 18:37:53 -0400

    updated files

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,21 +1,46 @@
 var animals = ["dog", "cat", "rat", "tiger", "lion"];
 
-for (var i = 0; i < animals.length; i++) {
-  var button = $("<button>");
-  button.attr("data-name", animals[i]);
-  button.addClass("animals");
-  button.text(animals[i]);
-  $("#buttons").append(button);
+function displaybuttons(animals) {
+  $("#buttons").empty();
+  for (var i = 0; i < animals.length; i++) {
+    var button = $("<button>");
+    button.attr("data-name", animals[i]);
+    button.addClass("animals");
+    button.text(animals[i]);
+    $("#buttons").append(button);
+  }
+
+  $(".animals").on("click", function() {
+    var displaygif = $(this).attr("data-name");
+    console.log("displaygif : " + displaygif + "\n");
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      displaygif +
+      "&api_key=Ro3GnmGgQXHjzG3RdDqaOdIUxdDLPoO3&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "get"
+    })
+      .then(function(response) {
+        console.log(response);
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+          var gifanimal = $("<img>");
+          gifanimal.attr("src", results[i].images.fixed_width.url);
+          $("#gif").prepend(gifanimal);
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
 }
 
-$.ajax({
-  url:
-    "https://api.giphy.com/v1/gifs/search?q=dog&api_key=Ro3GnmGgQXHjzG3RdDqaOdIUxdDLPoO3&limit=10",
-  method: "get"
-})
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+$("#submit").on("click", function() {
+  var newanimal = $("#newanimal").val();
+  console.log("newanimal", newanimal);
+  animals.push(newanimal);
+  console.log(animals);
+  displaybuttons(animals);
+});
+displaybuttons(animals);
