bd85131e0b6669e4919e82750fe67a88dfdd39d0
Neeraja <neerajareddy24@gmail.com>
2018-07-18 22:33:11 -0400

    updated files

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,46 +1,93 @@
-var animals = ["dog", "cat", "rat", "tiger", "lion"];
+var audioElement = document.createElement("audio");
 
-function displaybuttons(animals) {
+var cartoonCharacters = [
+  "Bugs Bunny",
+  "Mickey Mouse",
+  "Tweety",
+  "Popeye",
+  "Daffy Duck",
+  "Donald Duck",
+  "Goofy",
+  "Mogli",
+  "Yogi Bear",
+  "Winnie-the-Pooh",
+  "Superman",
+  "Olive Oly",
+  "Tom Cat",
+  "Jerry Mouse",
+  "Sponge Bob"
+];
+
+function displaybuttons(cartoonCharacters, classToAdd) {
   $("#buttons").empty();
-  for (var i = 0; i < animals.length; i++) {
+  for (var i = 0; i < cartoonCharacters.length; i++) {
     var button = $("<button>");
-    button.attr("data-name", animals[i]);
-    button.addClass("animals");
-    button.text(animals[i]);
+    button.addClass(classToAdd);
+    button.text(cartoonCharacters[i]);
     $("#buttons").append(button);
   }
-
-  $(".animals").on("click", function() {
-    var displaygif = $(this).attr("data-name");
-    console.log("displaygif : " + displaygif + "\n");
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      displaygif +
-      "&api_key=Ro3GnmGgQXHjzG3RdDqaOdIUxdDLPoO3&limit=10";
-    $.ajax({
-      url: queryURL,
-      method: "get"
-    })
-      .then(function(response) {
-        console.log(response);
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-          var gifanimal = $("<img>");
-          gifanimal.attr("src", results[i].images.fixed_width.url);
-          $("#gif").prepend(gifanimal);
-        }
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  });
 }
+$(document).on("click", ".cartoonCharacters", function() {
+  var displaygif = $(this).text();
+
+  console.log("displaygif : " + displaygif + "\n");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    displaygif +
+    "&api_key=Ro3GnmGgQXHjzG3RdDqaOdIUxdDLPoO3&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "get"
+  })
+    .then(function(response) {
+      console.log(response);
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifdiv = $("<div>");
+        gifdiv.addClass("gif-item");
+        var rating = results[i].rating;
+        var p = $("<p>");
+        p.text(rating);
+        animate = results[i].images.fixed_height.url;
+        still = results[i].images.fixed_height_still.url;
+
+        var gifcartoon = $("<img>");
+        gifcartoon.attr("src", still);
+        gifcartoon.attr("data-animate", animate);
+        gifcartoon.attr("data-still", still);
+        gifcartoon.attr("data-state", "still");
+        gifcartoon.addClass("gifcartoon");
+        gifdiv.prepend(p);
+
+        gifdiv.prepend(gifcartoon);
+        $("#gif").prepend(gifdiv);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+$(document).on("click", ".gifcartoon", function() {
+  currentstate = $(this).attr("data-state");
+  console.log(currentstate);
+  if (currentstate === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+    audioElement.setAttribute("src", "assets/music/laugh.mp3");
+    audioElement.loop = true;
+    audioElement.play();
+    console.log("animate condition");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+    audioElement.pause();
+  }
+});
 
 $("#submit").on("click", function() {
-  var newanimal = $("#newanimal").val();
-  console.log("newanimal", newanimal);
-  animals.push(newanimal);
-  console.log(animals);
-  displaybuttons(animals);
+  var newcharacter = $("#newcharacter").val();
+  cartoonCharacters.push(newcharacter);
+  displaybuttons(cartoonCharacters, "cartoonCharacters");
 });
-displaybuttons(animals);
+displaybuttons(cartoonCharacters, "cartoonCharacters");
